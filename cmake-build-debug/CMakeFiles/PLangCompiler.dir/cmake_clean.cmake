5efa69d2ca43111277fc88e775b50d5ae225fdc2
Ramon Brand <ramon2009@gmail.com>
2017-10-02 10:49:51 +0300

    Initial commit -> Lexer initial version.

---
+++ cmake-build-debug/CMakeFiles/PLangCompiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PLangCompiler.dir/main.cpp.obj"
+  "PLangCompiler.pdb"
+  "PLangCompiler.exe"
+  "PLangCompiler.exe.manifest"
+  "libPLangCompiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PLangCompiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
